2dd8be2d7f1a4e04989cfb3bcb8726814f4bddf8
Tung Teng <thetungctn@gmail.com>
2026-01-08 21:15:26 +0700

    Setup gsap plugins & lenis

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+import Lenis from 'lenis'
+
 import Navbar from './Navbar'
 import Intro from './Intro'
 import Slider from './Slider'
 import Outro from './Outro'
 
+gsap.registerPlugin(ScrollTrigger, SplitText)
+
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis()
+    lenis.on('scroll', ScrollTrigger.update)
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000)
+    })
+    gsap.ticker.lagSmoothing(0)
+  }, [])
+
   return (
     <main>
       <Navbar />
