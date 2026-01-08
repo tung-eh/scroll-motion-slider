382b830d05cc4e0372b9fc691e90d6b10f0c898a
Tung Teng <thetungctn@gmail.com>
2026-01-08 14:01:34 +0700

    Intro & Outro style

--- src/App/Outro.tsx
+++ src/App/Outro.tsx
@@ -1,7 +1,7 @@
 const Outro = () => {
   return (
-    <section>
-      <h1 className="text-[3rem] tracking-[-0.1rem] leading-[1.2]">
+    <section className="bg-[#0f0f0f] h-screen w-full text-white content-center p-8">
+      <h1 className="text-[3rem] tracking-[-0.1rem] leading-[1.2] lg:w-1/2 lg:mx-auto lg:text-center">
         As the sequence slows the silence takes over, holding the last traces of
         motion in the air.
       </h1>
